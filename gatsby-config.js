d1cf160e2c81c2623ec4d27d982e4fac007748bb
Yan Te <yan.te@team.wrike.com>
2017-11-08 18:59:38 +0300

    removed pathPrefix for hosting in root

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `halfof8`
   },
-  pathPrefix: `/halfof8`,
+  // pathPrefix: `/halfof8`,
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
