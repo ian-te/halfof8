1faa36c66da2aa9f456b685174dd6c53ac8446d0
Yan Te <yan.te@team.wrike.com>
2017-11-08 18:49:31 +0300

    removed unnecessary plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,21 +6,5 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-yaml`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`
-      }
-    }
   ]
 };
