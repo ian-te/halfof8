1cd1c05e0d0ae5bf1e7f314b5ab50903dc8e99ce
Yan Te <yan.te@team.wrike.com>
2017-11-15 10:01:59 +0300

    added spinner

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,22 @@ module.exports = {
         `gatsby-plugin-styled-components`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sharp`,
-        "gatsby-plugin-catch-links",
+        // "gatsby-plugin-catch-links",
         `gatsby-transformer-remark`,
         {
             resolve: `gatsby-source-contentful`,
             options: {
                 spaceId: process.env.spaceId,
                 accessToken: process.env.accessToken
+            }
+        },
+        {
+            resolve: `gatsby-plugin-nprogress`,
+            options: {
+                // Setting a color is optional.
+                color: `tomato`,
+                // Disable the loading spinner.
+                showSpinner: false
             }
         }
     ]
