22233d58508e37b0803974fbd61062b0c6e170b9
halfof8 <jetixc@gmail.com>
2018-01-16 12:03:24 +0300

    test3

--- src/components/Sidebar/logo/index.js
+++ src/components/Sidebar/logo/index.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 
 const Logo = ({ className }) => (
   <svg
-    className={className}
     xmlns="http://www.w3.org/2000/svg"
     width="150"
     height="150"
