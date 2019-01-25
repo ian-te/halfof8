e32e33b6fae79d95f279f6ce21f5803c7928e6b4
Yan Te <jante1989@gmail.com>
2019-01-25 08:58:39 +0300

    removed video overlay

--- src/layouts/inner.js
+++ src/layouts/inner.js
@@ -170,7 +170,6 @@ export const pageQuery = graphql`
                     secondaryTag
                     backgroundColor
                     textColor
-                    videoOverlay
                     body {
                         id
                         childMarkdownRemark {
