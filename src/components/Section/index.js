7588af6e922998a49554b788f176fe05fef27be0
Yan Te <jante1989@gmail.com>
2020-01-12 21:37:26 +0100

    flex instead of grid

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -10,35 +10,27 @@ export const Col = styled.div`
   background-color: transparent;
   overflow: hidden;
   background-image: url(${props => props.background});
-  @supports(display: grid) {
-    grid-column: span ${props => props.cols};
-    border-radius: 4px;
-  }
   height: 512px;
+  flex-grow: 1;
+  margin: 5px;
+  overflow: hidden;
+  border-radius: 4px;
+  width: calc(${props => props.cols * 25}% - 10px); 
   @media(max-width: 900px) {
-    grid-column:  span ${props => max(props.cols, 2)};
-    height: calc(100vw - 20px);
+    width: calc(${props => props.cols >= 2 ? 100 : 50}% - 10px); 
     max-height: 512px;
+    height: calc(100vw - 20px);
   }
+  
 `
 
 export const Section = styled.div`
   display: flex;
-  /*height: 512px;*/
   width: 100%;
-  justify-content: left;
+  justify-content: stretch;
   background-color: ${ props => props.bg || '#fff' };
   align-items: stretch;
   flex-wrap: wrap;
-  @supports(display: grid) {
-    display: grid;
-    grid-gap: 8px;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-  }
-  @media(max-width: 900px) {
-    grid-template-columns: 1fr 1fr;
-    // grid-template-columns: repeat(1fr, minmax(1fr, 2fr));
-  }
   max-width: 1960px;
   margin: 0 auto;
 `
