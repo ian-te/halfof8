6706283f69c74c5484bcc34eb1999f0e6d0c44b2
Halfof8 <jetixc@gmail.com>
2020-01-23 04:51:10 +0300

    fix of h2 padding on index (to 12px)

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -9,7 +9,7 @@ import icon_inner from "./icon_inner.svg";
 import VisibilitySensor from "react-visibility-sensor";
 const Description = styled.div`
     max-width: 250px;
-    margin-top: 22px;
+    margin-top: 12px;
     box-sizing: border-box;
     h2 {
         line-height: 1.4;
